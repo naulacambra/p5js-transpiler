d9b53db0c540d8f03304267949014ce1932b901b
Arnau Lacambra <naulacambra@gmail.com>
2019-07-23 15:04:44 +0200

    Improved transpiling - https://processing.org/examples/#structure

--- src/utils/processing_to_p5.js
+++ src/utils/processing_to_p5.js
@@ -1,9 +1,5 @@
 const rules = [
   {
-    from: /void (\w*)\(\)/gm,
-    to: "function $1()"
-  },
-  {
     from: /size\((\d+),\s?(\d+)\)/gm,
     to: "createCanvas($1, $2)"
   },
@@ -12,12 +8,31 @@ const rules = [
     to: "console.log($1)"
   },
   {
-    from: /(int|float|double|long|char|String|Array)\s/gm,
-    to: "let "
+    from: /(int|float|double|long|char|String|Array|PGraphics)\s(\w*)(;|\s=)/gm,
+    to: "let $2$3"
+  },
+  {
+    from: /(int|float|double|long|char|String|Array)\s(\w*)([,)])/gm,
+    to: "$2$3"
+  },
+  {
+    from: /void (\w*)\((.*)\)/gm,
+    to: "function $1($2)"
+  },
+  {
+    from: /(\w*\.beginDraw\(\))/gm,
+    to: "// $1"
+  },
+  {
+    from: /(\w*\.endDraw\(\))/gm,
+    to: "// $1"
   }
 ];
 
 export default function(content) {
+  if (!/setup/gm.test(content)) {
+    content = `function setup() { \n${content}\n}`;
+  }
   rules.forEach(r => (content = content.replace(r.from, r.to)));
   return content;
 }
