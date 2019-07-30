05c208e445df1f9a97cff795f6c999cb7e45b7aa
Arnau Lacambra <naulacambra@gmail.com>
2019-07-30 21:10:58 +0200

    Added navigation and toolbar

---
+++ src/layout/navigation.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-navigation-drawer v-model="drawer" app>
+    <v-list dense>
+      <v-list-item @click="() => {}">
+        <v-list-item-action>
+          <v-icon>home</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <router-link to="/">
+            <v-list-item-title>Editor</v-list-item-title>
+          </router-link>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item @click="() => {}">
+        <v-list-item-action>
+          <v-icon>contact_mail</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <router-link to="/examples">
+            <v-list-item-title>Examples</v-list-item-title>
+          </router-link>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script>
+export default {
+  props: {
+    value: { type: Boolean, default: null }
+  },
+  data() {
+    return {
+      drawer: this.value
+    };
+  },
+  watch: {
+    value(nv) {
+      this.drawer = nv;
+    },
+    drawer(nv) {
+      this.$emit("input", nv);
+    }
+  }
+};
+</script>
+
--- src/layout/toolbar.vue
+++ src/layout/toolbar.vue
@@ -1,10 +1,26 @@
 <template>
-  <v-toolbar></v-toolbar>
+  <v-app-bar app color="indigo" dark>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    <v-toolbar-title>p5.js transpiler</v-toolbar-title>
+  </v-app-bar>
 </template>
 <script>
 export default {
   props: {
-    app: { type: Object, default: () => {} }
+    value: { type: Boolean, default: null }
+  },
+  data() {
+    return {
+      drawer: this.value
+    };
+  },
+  watch: {
+    value(nv) {
+      this.drawer = nv;
+    },
+    drawer(nv) {
+      this.$emit("input", nv);
+    }
   }
 };
 </script>
